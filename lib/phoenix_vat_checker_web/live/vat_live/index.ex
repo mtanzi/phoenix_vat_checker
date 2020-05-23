3abdf3b21224d96433873a6ed952a3c24f1ce412
marco <tanzi.marco@gmail.com>
2020-05-23 20:41:35 +0200

    Add validation

--- lib/phoenix_vat_checker_web/live/vat_live/index.ex
+++ lib/phoenix_vat_checker_web/live/vat_live/index.ex
@@ -1,8 +1,20 @@
 defmodule PhoenixVatCheckerWeb.VatLive.Index do
   use PhoenixVatCheckerWeb, :live_view
 
+  alias ExVatcheck.VAT
+
   @impl true
   def mount(_params, _session, socket) do
     {:ok, assign(socket, query: nil, result: nil, valid: false, loading: false)}
+  end
+
+  def handle_event("suggest", %{"q" => query}, socket) when byte_size(query) <= 100 do
+    case ExVatcheck.Countries.valid_format?(query) do
+      true ->
+        {:noreply, assign(socket, query: query, valid: true, loading: true)}
+
+      false ->
+        {:noreply, assign(socket, query: query, valid: false, loading: true)}
+    end
   end
 end
