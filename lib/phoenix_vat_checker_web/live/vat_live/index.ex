31dc8ba3fff70fe1c2d86a6a3f823b71cc359a8c
marco <tanzi.marco@gmail.com>
2020-05-23 20:47:13 +0200

    Add call to wenservice

--- lib/phoenix_vat_checker_web/live/vat_live/index.ex
+++ lib/phoenix_vat_checker_web/live/vat_live/index.ex
@@ -17,4 +17,20 @@ defmodule PhoenixVatCheckerWeb.VatLive.Index do
         {:noreply, assign(socket, query: query, valid: false, loading: true)}
     end
   end
+
+  def handle_event("search", %{"q" => query}, socket) when byte_size(query) <= 100 do
+    send(self(), {:search, query})
+    {:noreply, assign(socket, query: query, result: nil, loading: true)}
+  end
+
+  def handle_info({:search, query}, socket) do
+    {:noreply, assign(socket, loading: false, result: fetch_vat(query), loading: false)}
+  end
+
+  defp fetch_vat(query) do
+    ExVatcheck.check(query)
+    |> handle_vat
+  end
+
+  defp handle_vat(%ExVatcheck.VAT{vies_response: result}), do: result
 end
